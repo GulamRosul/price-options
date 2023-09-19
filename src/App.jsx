a588d5f08970003854105dbacf0a935e228b0309
rosol111 <gulamrosul037@gmail.com>
2023-09-19 18:08:30 +0600

    added by Axios, data extraction and data processing  bar chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import LineChart from "./Coponents/LineChart/LineChart";
 import Navbar from "./Coponents/Navbar/Navbar";
+import Phones from "./Coponents/Phones/Phones";
 import PriceOptions from "./Coponents/PriceOptions/PriceOptions";
 
 // import DaisyNav from "./Coponents/DaisyNav/DaisyNav";
@@ -13,6 +14,7 @@ function App() {
       <h1 className="text-4xl bg-rose-600">Vite + React</h1>
       <PriceOptions></PriceOptions>
       <LineChart></LineChart>
+      <Phones></Phones>
     </>
   );
 }
